3a15bfb986181f5499dbdfc32ce026f2b1c17a8f
Kiran K <mailtokirankk@gmail.com>
2022-03-28 13:31:30 +0530

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from "next";
+import Container from "../components/Container";
+
+const Home: NextPage = () => {
+  return (
+    <Container title="Home">
+      <div className="space-y-4">
+        <h2 className="text-2xl">BoxyHQ SAML Example</h2>
+        <p>
+          This is an example website to demonstrate how to use <strong>BoxyHQ SAML</strong> for SAML SSO authentication.
+        </p>
+        <p className="text-gray-600">
+          You can use{" "}
+          <a href="https://mocksaml.com/" className="underline underline-offset-2 font-medium">
+            Mock SAML
+          </a>{" "}
+          a free SAML 2.0 Identity Provider to test the SAML SSO integration.
+        </p>
+        <ul className="flex flex-row space-x-6">
+          <li>
+            <a className="underline underline-offset-2" target="_blank" rel="noreferrer" href="https://boxyhq.com/docs/jackson/introduction">
+              Documentation
+            </a>
+          </li>
+          <li>
+            <a className="underline underline-offset-2" target="_blank" rel="noreferrer" href="https://github.com/boxyhq/jackson">
+              Github
+            </a>
+          </li>
+          <li>
+            <a className="underline underline-offset-2" target="_blank" rel="noreferrer" href="https://www.npmjs.com/package/@boxyhq/saml-jackson">
+              NPM
+            </a>
+          </li>
+          <li>
+            <a className="underline underline-offset-2" target="_blank" rel="noreferrer" href="https://hub.docker.com/r/boxyhq/jackson">
+              Docker Hub
+            </a>
+          </li>
+        </ul>
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
